cf59d9375191b04ca0fea90fce0383b258428bbb
KUN4L-S4HU <114688977+KUN4L-S4HU@users.noreply.github.com>
2024-11-17 14:21:24 +0530

    Update

---
+++ star.py
@@ -0,0 +1,15 @@
+import turtle
+sc=turtle.Screen()
+sc.setup(600,600)
+spiral=turtle.Turtle()
+spiral.speed(20)
+sc.bgcolor('black')
+col=['yellow','blue','white','green']
+c=0
+for i in range(60):
+    spiral.forward(i*10)
+    spiral.right(144)
+    spiral.color(col[c])
+    if c==3:
+            c+=1
+            turtle.done
